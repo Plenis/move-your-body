571a4fe68f007bd6bdb5bf250df7ccf49dde8334
Sino Plenis <plenissino@gmail.com>
2020-12-10 17:24:21 +0200

    App file changes

--- src/main/java/net/webApp/App.java
+++ src/main/java/net/webApp/App.java
@@ -27,7 +27,6 @@ public class App {
         }
         return 4567;
     }
-<<<<<<< HEAD
 
     static Jdbi getJdbiDatabaseConnection(String defaultJdbcUrl) throws URISyntaxException {
         ProcessBuilder processBuilder = new ProcessBuilder();
@@ -64,23 +63,18 @@ public class App {
             ExerciseDaoImpl exerciseDao = new ExerciseDaoImpl(jdbi);
             PlayerDaoImpl playerDao = new PlayerDaoImpl(jdbi);
             Map<String, Object> player = new HashMap<>();
-=======
->>>>>>> ababf854046687a0df700e95a4cfbcd52899d7d6
 
-    static Jdbi getJdbiDatabaseConnection(String defaultJdbcUrl) throws URISyntaxException {
-        ProcessBuilder processBuilder = new ProcessBuilder();
-        String database_url = processBuilder.environment().get("DATABASE_URL");
-        if (database_url != null) {
+        get("/", (request, response) -> {
 
-            URI uri = new URI(database_url);
-            String[] hostParts = uri.getUserInfo().split(":");
-            String username = hostParts[0];
-            String password = hostParts[1];
-            String host = uri.getHost();
+            return new ModelAndView(player, "index.handlebars");
 
-            int port = uri.getPort();
+        }, new HandlebarsTemplateEngine());
 
-<<<<<<< HEAD
+        get("/tester", (request, response) -> {
+            return new ModelAndView(player, "tester.handlebars");
+        }, new HandlebarsTemplateEngine());
+
+
        get("/player/:id", (request, response) -> {
           Map<String, Object> map = new HashMap<>();
 
@@ -133,36 +127,5 @@ public class App {
             get("/motion", (request, response) -> {
                 return new ModelAndView(player, "motion.handlebars");
             }, new HandlebarsTemplateEngine());
-=======
-            String path = uri.getPath();
-            String url = String.format("jdbc:postgresql://%s:%s%s", host, port, path);
->>>>>>> ababf854046687a0df700e95a4cfbcd52899d7d6
-
-            return Jdbi.create(url, username, password);
-        }
-
-        return Jdbi.create(defaultJdbcUrl);
-    }
-
-    public static void main(String[] args) {
-
-        try {
-
-            port(getHerokuAssignedPort());
-            staticFiles.location("/public");
-
-            Jdbi jdbi = getJdbiDatabaseConnection("jdbc:postgresql://localhost/waiters_app?username=sino&password=123");
-
-            Map<String, Object> waiter = new HashMap<>();
-
-            get("/", (request, response) -> {
-
-                return new ModelAndView(waiter, "login.handlebars");
-
-            }, new HandlebarsTemplateEngine());
-
-        } catch (URISyntaxException e) {
-            e.printStackTrace();
-        }
     }
 }
